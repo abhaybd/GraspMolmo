1778361fd827b9d449b2f115017c09f6daef4a1d
Abhay D <a.deshpande012@gmail.com>
2025-05-15 12:51:44 -0700

    Minor cleanup

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,5 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 $(document).ready(function() {
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-    }
 
-		// Initialize all div with carousel class
-    bulmaCarousel.attach('.carousel', options);
 })

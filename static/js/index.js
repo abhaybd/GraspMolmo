56b09dc915d4a99a1b5ae103bcbb44f3b0db670a
Abhay D <a.deshpande012@gmail.com>
2025-05-15 13:56:03 -0700

    Add speed label to videos, fix browser video caching

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,14 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 $(document).ready(function() {
-
+  // Add a timestamp to all video sources to prevent browser caching
+  const allVideos = document.querySelectorAll('video');
+  for (const video of allVideos) {
+    video.pause();
+    for (const source of video.getElementsByTagName('source')) {
+      source.src += '?' + new Date().getTime();
+    }
+    video.load();
+    video.play();
+  }
 })
